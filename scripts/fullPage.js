a12fd551a20f6280dffe8e5333351b8b2097ac57
omar <omar@OMAR-PC>
2018-01-19 17:59:52 +0000

    initial commit

---
+++ scripts/fullPage.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      navigation: true,
+	});
+	$("#abir").click(function() {
+		
+	});
+});
+function moveIt() {
+	console.log("hey");
+}
